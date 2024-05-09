8b75c51207c5f95bc169790259ad58ce261bfc61
Franklin <franklin.op@hotmail.com>
2024-05-09 13:56:46 +0200

    added base components and styling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,9 +3,10 @@
         FILTER
     </section>
     <section>
+        <base-card>
         <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register"> Register as Coach </router-link>
+            <base-button mode="outline">Refresh</base-button>
+            <base-button link to="/register"> Register as Coach </base-button>
         </div>
         <ul v-if="hasCoaches"> 
            <coach-item
@@ -19,18 +20,20 @@
               ></coach-item>
         </ul>
         <h3 v-else> No coaches found. </h3>
-       
+          </base-card>
     </section>
 </template>
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue'
+import BaseButton from '../../components/ui/BaseButton.vue';
 
 
 
 export default {
     components: {
-        CoachItem
+        CoachItem,
+        BaseButton
     },
     computed: {
         filteredCoaches(){
