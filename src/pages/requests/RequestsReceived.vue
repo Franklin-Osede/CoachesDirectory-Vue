ebe75861a9de7ecc31063118dc583d4da69c3497
Franklin <franklin.op@hotmail.com>
2024-05-11 13:20:07 +0200

    successfully sending coaching request http ans save on firebase

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,4 +1,7 @@
 <template>
+ <base-dialog :show="!!error" title="An error ocurred!" @close="handleError">
+ <p> {{error}} </p>
+ </base-dialog>
   <section>
     <base-card>
     <header>
@@ -6,7 +9,8 @@
         Requests Received
       </h2>
     </header>
-    <ul v-if="hasRequests">
+    <base-spinner v-if="isLoading"></base-spinner>
+    <ul v-else-if="hasRequests && !isLoading">
     <request-item
      v-for="req in receivedRequests"
      :key="req.id"
@@ -26,12 +30,35 @@ export default {
   components: {
     RequestItem
   },
+  data (){
+    return {
+      isLoading: false,
+      error: null
+    }
+  },
   computed: {
     receivedRequests(){
      return this.$store.getters['requests/requests']
     },
     hasRequests(){
       return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  created(){
+    this.loadRequests();
+  },
+  methods: {
+   async loadRequests (){
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed!';
+      }
+     this.isLoading = false;
+    },
+    handleError(){
+      this.error = null
     }
   }
 }
