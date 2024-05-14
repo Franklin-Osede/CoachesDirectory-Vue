f63f59f1032410c3f11f220bcbbc8a8de7c213b6
Franklin <franklin.op@hotmail.com>
2024-05-14 10:44:41 +0200

    updated actions sectiom

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -31,36 +31,38 @@ export default {
         });
     },
     async loadCoaches(context, payload) {
-        if (!payload.forceRefresh && !context.getters.shouldUpdate){
-            return;
+        if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+          return;
         }
+      
+        const response = await fetch(`https://coach-finder-5b5d6-default-rtdb.firebaseio.com/coaches.json`)
 
-        const response = await fetch (
-            `https://coach-finder-5b5d6-default-rtdb.firebaseio.com/coaches.json`
-            );
+      
+        if (!response.ok) {
+          const errorData = await response.json(); // Parse error response
+          const error = new Error(errorData.message || 'Failed to fetch!');
+          throw error;
+        }
+      
         const responseData = await response.json();
-
-        if(!response.ok){
-            const error = new Error (responseData.message || 'Failed to fetch!');
-            throw error;
-         }
-        
-    
+        console.log("jjj........", responseData)
+      
         const coaches = [];
-
-        for (const key in responseData){
-            const coach = {
-                id: key,
-                firstName: responseData[key].firstName,
-                lastName: responseData[key].lastName,
-                description: responseData[key].description,
-                hourlyRate:responseData[key].hourlyRate,
-                areas: responseData[key].areas,
-            };
-            coaches.push(coach);
+      
+        for (const key in responseData) {
+          const coach = {
+            id: key,
+            firstName: responseData[key].firstName,
+            lastName: responseData[key].lastName,
+            description: responseData[key].description,
+            hourlyRate: responseData[key].hourlyRate,
+            areas: responseData[key].areas
+          };
+          coaches.push(coach);
         }
-
+      
         context.commit('setCoaches', coaches);
         context.commit('setFetchTimestamp');
-    }
-}
+      }
+      
+    };
